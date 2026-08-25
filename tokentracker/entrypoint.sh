e720b966bd27ee2dcd25f172ed8d7f43436710d0
papatenko <justinkondratenko@proton.me>
2026-08-25 16:22:53 -0500

    Revert "Reapply "Merge pull request #90 from papatenko/feat/add-tokentracker-stack""
    This reverts commit bf5f505f158a187812153cfeb50fde35e0db3831.

--- tokentracker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Runs as root: bind-mounted volumes arrive owned by the host UID, so reclaim
-# them for the unprivileged tokentracker user before dropping root and
-# handing off to run-tracker.sh.
-set -eu
-
-chown -R tokentracker:tokentracker /home/tokentracker
-
-exec su-exec tokentracker /usr/local/bin/run-tracker.sh
